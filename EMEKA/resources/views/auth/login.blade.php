62e53ca14068d0149605a834df3a004d573e675b
versatile153 <ceze46321@gmail>
2023-05-12 23:15:22 -0700

    se

--- EMEKA/resources/views/auth/login.blade.php
+++ EMEKA/resources/views/auth/login.blade.php
@@ -1,6 +1,362 @@
-@extends('layouts.app')
 
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+<script type="text/javascript" src="https://cdn.cookielaw.org/consent/02abb198-81a8-49e5-a9b1-f69a5dd9c039/OtAutoBlock.js"></script>
+<script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" charset="UTF-8" data-domain-script="02abb198-81a8-49e5-a9b1-f69a5dd9c039"></script>
+
+<script>
+function OptanonWrapper(){var t;window.infotrackInitialized||void 0===window.Infotrack?window.Infotrack.setSegmentIntegrations():(window.infotrackInitialized=!0,Infotrack.load("https://g.foolcdn.com/mms/resources/js/infotrack-ot_min.js"),t=t||0,setTimeout(function(){window.Infotrack.initialize("marketing")},t))}window.top!=window&&(window.analytics=window.top.analytics,window.inIframe=!0);var segmentKey="AGH47f5dTWztXZ8odR03AAD5yaGd2Oyr",segmentSnippetVersion="4.15.2",getSegmentUrl=function(t){return t=t||window.segmentKey,("https:"===document.location.protocol?"https://":"http://")+"evs.pink-boat.fool.com/analytics.js/v1/"+t+"/analytics.min.js"},trackerMaker=function(t){function n(n){function t(){var t=Array.prototype.slice.call(arguments,0),t=[n].concat(t);e.push(t)}return t.stub=!0,t}for(var e=[],o=(e.invoked=!1,e.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"],t&&(e.methods=e.methods.concat(t)),0);o<e.methods.length;o++){var i=e.methods[o];e[i]=n(i)}return e.load=function(t){var n;e.invoked?window.console&&console.error&&console.error("Tracking snippet included twice."):((n=document.createElement("script")).type="text/javascript",n.async=!0,n.src=t,(t=document.getElementsByTagName("script")[0]).parentNode.insertBefore(n,t),e.invoked=!0)},e},analytics=window.analytics=window.analytics||trackerMaker(["addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"]),Infotrack=window.Infotrack=window.Infotrack||trackerMaker(["initialize","setSegmentIntegrations"]);
+    </script>
+<meta name="infotrackSnippetVersion" content="7.3.1" data-tracker-key="infotrackSnippetVersion">
+<meta charset="utf-8">
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="page_type" content="ecap" data-tracker-key="page_type">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<style>.async-hide { opacity: 0 !important} </style> <script> var dataLayer = dataLayer || []; (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date; h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')}; (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c; })(window,document.documentElement,'async-hide','dataLayer',4000, {'GTM-TRKG9Q3':true}); </script>
+<link rel="preload" href="/static/mms/css/base_template.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+<link rel="preload" href="/static/mms/css/ck_editor.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+<style>
+        #header {
+            background-color: #FFFFFF;
+            padding: 15px;
+            text-align: center;
+            left: 0;
+            top: 0;
+            width: 100%;
+            z-index: 99999;
+            position: -webkit-sticky; /* Safari */
+            position: sticky;
+            box-shadow: 0px 4px 6px -4px black;
+        }
+
+        header {
+            height: 50px;
+        }
+
+        body {
+            background-color: #f3f3f5;
+            padding-bottom: 40px;
+            font-family: Proxima Nova, Roboto, Helvetica, sans-serif;
+            background-size: cover;
+            background-position-x: center;
+            background-image : url("https://g.foolcdn.com/misc-assets/background-version1.png");
+            color : rgb(29, 26, 116);
+            background-repeat : no-repeat;
+        }
+
+        .container {
+            max-width: 940px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        .container .col {
+            max-width: 66%;
+            display: block;
+            float: none !important;
+            padding: 50px 30px 0 0;
+        }
+
+        h1 {
+            margin: 10px 0 20px;
+            line-height: 40px
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 24px;
+            padding-right: 20px;
+        }
+
+        label, select, button {
+            cursor: text;
+        }
+
+        div.subtext {
+            margin-top: 100px;
+            border-top: 2px solid #BBBBBB;
+            font-family: verdana,sans-serif;
+            padding: 8px 0 0;
+        }
+
+        .disclosure > p {
+            font-size: 10px;
+            line-height: 14px;
+            padding-right: 40px;
+        }
+
+        a {
+            color: #00699d;
+        }
+
+        @media (max-width: 979px) {
+            .container .col {
+                max-width: unset;
+                text-align: center;
+                padding-left: 30px;
+                padding-right: 0;
+                padding-top: 30px;
+            }
+
+            body {
+                padding-left: 0px;
+                padding-right: 0px;
+                background-image: none;
+            }
+
+            header {
+                height: unset;
+            }
+
+            p {
+                padding: 0 20px;
+            }
+
+            .disclosure > p {
+                padding: 0;
+            }
+        }
+
+        @media (max-width: 767px) {
+            p {
+                padding: 0 20px;
+            }
+
+            .container .col {
+                max-width: unset;
+                text-align: center;
+                padding-right: 0;
+                padding-left: unset;
+            }
+
+            #header-image {
+                 width: 180px;
+            }
+
+            body {
+                padding-left: 0;
+                padding-right: 0;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .container .col {
+                max-width: 98%;
+                text-align: center;
+                padding-top: 30px;
+            }
+
+            #header-image {
+                 width: 140px;
+            }
+
+            h1 {
+                font-size: 24px;
+                line-height: 28px;
+            }
+
+            p, h3 {
+                font-size: 14px;
+                line-height: 18px;
+            }
+
+            body {
+                padding-left: 0;
+                padding-right: 0;
+            }
+
+            #header {
+                position: static;
+            }
+        }
+            #ecapWidgetForm input[type="email"] {
+                border: 1px solid #1d1a74 !important;
+            }
+
+            #ecapWidgetForm_submit {
+                display: inline-block;
+                padding: 8px 25px !important;
+                margin: 0 0.3em 0.3em 0 !important;
+                border-radius: 4px !important;
+                border-style: solid !important;
+                border-color: #0095CD !important;
+                border-width: 10px 18px !important;
+                background-image: linear-gradient(#0095CD, #0095CD) !important;
+                box-sizing: border-box;
+                text-decoration: none;
+                font-family: 'Proxima-Nova', 'Avenir Next', 'Avenir', Roboto, Helvetica, sans-serif !important;
+                color: #FFFFFF !important;
+                background-color: #0095CD !important;
+                box-shadow: 0 2px 3px rgb(0 0 0 / 16%) !important;
+                text-align: center;
+                position: relative;
+                font-size: 18px !important;
+                line-height: 1.1 !important;
+                font-weight: bold !important;
+                transition: all 100ms ease-in !important;
+            }
+
+            #ecapWidgetForm_submit:hover {
+	            background: #555555 !important;
+	            border-color: #555555 !important;
+            }
+
+
+
+
+
+
+
+
+
+
+
+            
+    </style>
+<script rel="preload" src="//g.foolcdn.com/mms/assets/js/lib/jquery/jquery.min.js" as="script" onload="this.onload=null;this.rel='script'"></script>
+<script type="text/javascript">
+    var dataLayer = dataLayer || [];
+    var md5HashedEmail = unescape(encodeURIComponent("d41d8cd98f00b204e9800998ecf8427e")).trim().toLowerCase();
+    var sha256HashedEmail = unescape(encodeURIComponent("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")).trim().toLowerCase();
+    dataLayer.push({'EmailReady.email': ''});
+    dataLayer.push({'EmailReady.hashed_email': md5HashedEmail});
+    dataLayer.push({'EmailReady.hashed_email_256': sha256HashedEmail});
+    dataLayer.push({'event': 'EmailReady'});
+    !function(l,a){a.liosetup=a.liosetup||{},a.liosetup.callback=a.liosetup.callback||[],a.liosetup.addEntityLoadedCallback=function(l,o){if("function"==typeof a.liosetup.callback){var i=[];i.push(a.liosetup.callback),a.liosetup.callback=i}a.lio&&a.lio.loaded?l(a.lio.data):o?a.liosetup.callback.unshift(l):a.liosetup.callback.push(l)}}(document,window);
+    window.liosetup.addEntityLoadedCallback(function(data){ dataLayer.push({'event': 'LyticsSegmentsReady', 'segments': data.segments}); });
+</script>
+<title data-swapper-tag='title'>
+        Get Access to The CapitalCrest's Latest Stock Picks
+    </title>
+<link rel="shortcut icon" type="image/png" href="//g.foolcdn.com/misc-assets/logo-tmf-favicon-7-magenta-royalpurple.ico" />
+</head>
+<body>
+<div id="header">
+<div class="row">
+<div class="col">
+<header>
+<img id="header-image" alt="The CapitalCrest logo" width="260px" src="https://g.foolcdn.com/misc-assets/tmf_holdingco_logo_tagline_alternate_magenta_royalpurple.png" />
+<div style="display:inline;vertical-align:-webkit-baseline-middle;" data-swapper-tag="swapper-header-image"></div>
+</header>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col">
+<main>
+<h1 id="headline" data-swapper-tag="title">Get Access to The CapitalCrest's Latest Stock Picks</h1>
+<div class="ck-content"> <p>Don't miss your chance to get in on the top 10 stocks we think are the best buys now inside our flagship online investing service <i>Stock Advisor</i>.</p><p>The average stock pick inside <i>Stock Advisor</i> is <strong>up 408% vs. just 120%</strong> for the S&amp;P.</p><p>Looking back five years from now, you'll probably wish you'd grabbed these stock picks.</p><p>If you want to take advantage of this buying opportunity, simply enter your email address below to access our secure sign-up page.</p> </div>
+<script src="//g.foolcdn.com/mms/assets/js/ecap-ajax.js?v=1"></script>
+<style>
+    .call-to-action {
+        max-width: 590px;
+        margin: 40px auto 20px;
+    }
+
+    #email-input {
+        display: block;
+        margin: 10px 0 0;
+    }
+
+    #ecapWidgetForm label {
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    #ecapWidgetForm input[type="email"] {
+        font-size: 18px;
+        color: #333333;
+        height: 46px;
+        padding: .5em;
+        width: 260px;
+        background-color: #ffffff;
+        border: 1px solid #cccccc;
+        border-radius: 4px;
+        -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+        -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+        box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+        -webkit-transition: border linear .2s, box-shadow linear .2s;
+        -moz-transition: border linear .2s, box-shadow linear .2s;
+        -o-transition: border linear .2s, box-shadow linear .2s;
+        transition: border linear .2s, box-shadow linear .2s;
+    }
+
+    #ecapWidgetForm input[type="email"]:focus {
+        border-color: rgba(82, 168, 236, 0.8);
+        outline: 0;
+        outline: thin dotted \9;
+        -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px rgb(82 168 236 / 60%);
+        -moz-box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(82,168,236,.6);
+        box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px rgb(82 168 236 / 60%);
+    }
+
+    #ecapWidgetForm input:focus:invalid:focus{
+        border-color: rgba(82, 168, 236, 0.8);
+        box-shadow: 0 1px 1px rgb(0 0 0 / 8%) inset, 0 0 8px rgb(82 168 236 / 60%);
+    }
+
+    #ecapWidgetForm input[type="submit"] {
+        width: auto;
+        -webkit-appearance: button;
+        cursor: pointer;
+    }
+
+    #ecapWidgetForm_submit {
+        display: inline-block;
+        padding: 1em 4em;
+        margin: 0 0.3em 0.3em 0;
+        border-radius: 0.75em;
+        box-sizing: border-box;
+        text-decoration: none;
+        font-family: 'Roboto', sans-serif;
+        color: #FFFFFF;
+        background-color: #3369ff;
+        box-shadow: inset 0 -0.6em 1em -0.35em rgb(0 0 0 / 17%), inset 0 0.6em 2em -0.3em rgb(255 255 255 / 15%), inset 0 0 0em 0.05em rgb(255 255 255 / 12%);
+        text-align: center;
+        position: relative;
+    }
+
+    #ecapWidgetForm .hlp-privacy {
+        font-size: 10px;
+        line-height: 14px;
+    }
+
+    .error {
+        color: #CD0A0A;
+    }
+
+    .ecapWidget_error {
+        background-color: #FEF1EC !important;
+        border: 1px solid #CD0A0A !important;
+    }
+
+    @media (max-width: 480px) {
+        #ecapWidgetForm_submit {
+            padding: 1em;
+            font-size: 16px;
+        }
+    }
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +426,73 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<p data-swapper-tag="ecap-disclosure" class="hlp-privacy">
+<div class="disclosure">
+<p data-swapper-tag="ecap-disclosure">
+By submitting your details, you consent to us keeping you informed about updates
+to our website and about other products and services that we think might interest you.
+You can unsubscribe at any time. Please read our
+<a href="https://www.fool.com/legal/privacy-statement/" target="_blank">Privacy Statement</a>
+and
+<a href="https://www.fool.com/legal/the-motley-fools-rules.aspx" target="_blank">Terms &amp; Conditions</a>.
+</p>
+</div>
+</p>
+</div>
+</form>
+</div>
+</div>
+<div class="subtext">
+<div class="disclosure">
+<div data-swapper-tag="before-disclosure"></div>
+<p>Returns are updated during market hours. The CapitalCrest has a <a href="http://www.fool.com/Legal/fool-disclosure-policy.aspx">disclosure policy</a>.</p>
+<p>
+The CapitalCrest respects your privacy and strives to be transparent about our data collection practices.
+We use your information to customize the site for you, to contact you about your membership, provide you with
+promotional information, and in aggregate to help us better understand how the service is used.
+</p>
+<p>
+Past performance is not a predictor of future results.
+Individual investment results may vary.
+All investing involves risk of loss.
+</p>
+<div data-swapper-tag="disclosure-addon"></div>
+<p><a href="https://www.fool.com/PopUps/PrivacyInfo.htm" target="_blank">Privacy/Legal Information</a>.</p>
+</div>
+</div>
+</main>
+<footer>
+<p class="text-center">
+&copy;1995-2023 The CapitalCrest. All rights reserved. |
+<a href="https://www.fool.com/help/FoolMarks.htm" onclick="bNoPop=true" target="_blank">Legal Information</a> |
+<a href="https://www.fool.com/PopUps/PrivacyInfo.htm" target="_blank">Privacy Information</a>
+</p>
+</footer>
+</div>
+</div>
+</div>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon='{"rayId":"7c4bbc2cc88ad7b1","token":"e081e875f70442d4bdc4b24dde150ec9","version":"2023.4.0","si":100}' crossorigin="anonymous"></script>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script type="text/javascript" id="dataLayerCampaignHelper">
+        var dataLayer = dataLayer || [];
+        dataLayer.push({ 'campaign': 'sa-bbn-evergreen' });
+    </script>
+</html>
